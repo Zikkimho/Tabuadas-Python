939e38b8caf0775c0247cf70f3536e627a45854d
Diego <diegofloriano2006@gmail.com>
2025-05-21 21:23:27 -0400

    Add files via upload

---
+++ tabuada_s.py
@@ -0,0 +1,14 @@
+def tabuada(a, b):
+    for i in range(10):
+       b += 1
+       print(f"{a} x {b} = {a * b}")
+print("\nTabuada do número 1: ")
+tabuada(1 ,0)
+print("\nTabuada do número 2: ")
+tabuada(2 ,0)
+print("\nTabuada do número 3: ")
+tabuada(3 ,0)
+print("\nTabuada do número 4: ")
+tabuada(4 ,0)
+print("\nTabuada do número 5: ")
+tabuada(5 ,0)
